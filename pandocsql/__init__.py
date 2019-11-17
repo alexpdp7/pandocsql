d172444d8c030d1b8ffa031b2972458d3d7d3f5c
alex <alex@pdp7.net>
2019-11-17 11:31:35 +0100

    Add flake8/black styling and Travis

--- pandocsql/__init__.py
+++ pandocsql/__init__.py
@@ -5,9 +5,12 @@ import panflute as pf
 
 def bp():
     import sys
-    sys.stdin = open('/dev/tty')
+
+    sys.stdin = open("/dev/tty")
     import pdb
+
     pdb.set_trace()
+
 
 def prepare(doc):
     doc.sql = sqlite3.connect(":memory:")
@@ -17,12 +20,15 @@ def action(elem, doc):
     if isinstance(elem, pf.Table):
         table = elem.caption.list[0].text
         headers = list(map(pf.stringify, elem.header.content))
-        headers_sql = ','.join(headers)
-        placeholders_sql = ','.join(['?']*len(headers))
+        headers_sql = ",".join(headers)
+        placeholders_sql = ",".join(["?"] * len(headers))
         doc.sql.execute(f"create table {table} ({headers_sql})")
         for row in elem.content:
             row_values = [pf.stringify(cell) for cell in row.content]
-            doc.sql.execute(f"insert into {table}({headers_sql}) values ({placeholders_sql})", row_values)
+            doc.sql.execute(
+                f"insert into {table}({headers_sql}) values ({placeholders_sql})",
+                row_values,
+            )
         return []
     if isinstance(elem, pf.CodeBlock):
         if elem.classes != ["pandocsql"]:
@@ -30,22 +36,23 @@ def action(elem, doc):
         table = doc.sql.execute(elem.text)
         headers = [col[0] for col in table.description]
         rows = list(table)
+
         def c(t):
             return pf.TableCell(pf.Para(pf.Str(t)))
+
         def r(cs):
             return pf.TableRow(*list(map(c, cs)))
+
         return [pf.Table(*list(map(r, rows)), header=r(headers))]
+
 
 def finalize(doc):
     pass
 
 
 def main(doc=None):
-    return pf.run_filter(action,
-                         prepare=prepare,
-                         finalize=finalize,
-                         doc=doc) 
+    return pf.run_filter(action, prepare=prepare, finalize=finalize, doc=doc)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
